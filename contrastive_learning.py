68d64f44115623fb399b8e4a9f2a95f7907bb820
Narasimha Bonda <narasimha12712@gmail.com>
2024-06-16 09:43:14 +0900

    Initial commit

---
+++ contrastive_learning.py
@@ -0,0 +1,61 @@
+import datetime
+import argparse
+import torch
+from torch import nn
+import torchvision
+import pytorch_lightning as pl
+from pytorch_lightning.loggers import TensorBoardLogger
+from lightly.data import LightlyDataset
+from lightly.transforms import MoCoV2Transform
+
+from models.contrastive.moco import MocoModel
+
+# Fixed parameters
+path_to_train = "/home/legacy/data/cifar10/train"
+batch_size = 128
+num_workers = 8
+seed = 1
+max_epochs = 100
+precision = 16
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--model_name",
+        type=str,
+        default="moco",
+        help="[simclr, moco, swav, byol]",
+    )
+    return parser.parse_args()
+
+def main():
+    args = parse_args()
+    pl.seed_everything(seed)
+    resnet = torchvision.models.resnet18(weights="IMAGENET1K_V1")
+    backbone = nn.Sequential(*list(resnet.children())[:-1])
+    model = MocoModel(backbone)
+    transform = MoCoV2Transform(input_size=32, gaussian_blur=0.0)
+    dataset = LightlyDataset(input_dir=path_to_train, transform=transform)
+    dataloader = torch.utils.data.DataLoader(
+        dataset,
+        batch_size=batch_size,
+        shuffle=True,
+        drop_last=True,
+        num_workers=num_workers,
+    )
+    logger = TensorBoardLogger(
+        save_dir="tb_logs/contrastive",
+        name=args.model_name,
+        version=datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S"),
+    )
+    trainer = pl.Trainer(
+        max_epochs=max_epochs,
+        devices=1,
+        accelerator="gpu",
+        logger=logger,
+        precision="16-mixed",  # Corrected the precision setting
+    )
+    trainer.fit(model, dataloader)
+
+if __name__ == "__main__":
+    main()
